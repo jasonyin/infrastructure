525d2dd642189ab475e2fee1b83fa3435f1a9cfa
Jason Yin <jason.yin@oracle.com>
2023-04-03 10:31:13 -0700

    object storage

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 3.70.0"
+      source  = "oracle/oci"
+    }
+  }
+}
+
+provider "oci" {
+  private_key          = var.private_key
+  private_key_password = var.private_key_password
+}
+
+/*module "network" {
+  source = "./network"
+
+  compartment_id = var.compartment_id
+  tenancy_ocid   = var.tenancy_ocid
+
+  cidr_blocks            = local.cidr_blocks
+  ssh_managemnet_network = local.ssh_managemnet_network
+}
+
+module "compute" {
+  source     = "./compute"
+  depends_on = [module.network]
+
+  compartment_id      = var.compartment_id
+  tenancy_ocid        = var.tenancy_ocid
+  cluster_subnet_id   = module.network.cluster_subnet.id
+  permit_ssh_nsg_id   = module.network.permit_ssh.id
+  ssh_authorized_keys = var.ssh_authorized_keys
+
+  cidr_blocks = local.cidr_blocks
+}*/
+
+module "object_storage" {
+  source     = "./object_storage"
+  depends_on = [module.object_storage]
+
+  compartment_id      = var.compartment_id
+  tenancy_ocid        = var.tenancy_ocid
+  bucket_namespace    = var.bucket_namespace
+}
---
+++ terraform/object_storage/variables.tf
@@ -0,0 +1,10 @@
+variable "compartment_id" {
+  description = "OCI Compartment ID"
+  type        = string
+}
+
+variable "bucket_namespace" {
+  description = "bucket namespace"
+  type        = string
+  default     = "axxnd9gqk5wt"
+}
