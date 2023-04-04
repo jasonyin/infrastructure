c17702d84fc5ade33f8cdb2ad65d2dc094cdad12
Jason Yin <jason.yin@oracle.com>
2023-04-04 08:21:01 -0700

    remove tenant global variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,10 @@ terraform {
 
 provider "oci" {
   region = var.region
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid = var.user_ocid
-  fingerprint = var.fingerprint
-  private_key_path = var.private_key_path
+  #tenancy_ocid = var.tenancy_ocid
+  #user_ocid = var.user_ocid
+  #fingerprint = var.fingerprint
+  #private_key = var.private_key
 }
 
 /*module "network" {
