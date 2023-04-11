df83c0de4d139eb87aa51d3224546f614ddf54a2
Jason Yin <jason.yin@oracle.com>
2023-04-11 13:36:01 -0700

    update server worker scripts with waits

--- terraform/k3s_infra/cloud-init/scripts/server.template.sh
+++ terraform/k3s_infra/cloud-init/scripts/server.template.sh
@@ -9,14 +9,19 @@ iptables -I INPUT -i ens3 -p tcp -s 10.0.0.0/8  --dport 6443 -j ACCEPT
 
 curl -sfL https://get.k3s.io | K3S_TOKEN='${cluster_token}' sh -s - server --cluster-init
 
-
-# while ! nc -z localhost 6443; do
-#   sleep 1
-# done
+# check tcp to k3s and wait for 200s
+for ((i=0; i<=10; i++));
+do
+    nc -vzw20 server.public.main.oraclevcn.com 6443;
+    if [[ $? == 0 ]]
+    then
+        break
+    fi
+done
 
 mkdir /home/opc/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/opc/.kube/config
-sed -i "s/127.0.0.1/$(curl -s ifconfig.co)/g" /home/opc/.kube/config
+sed -i "s/127.0.0.1/$(hostname -i)/g" /home/opc/.kube/config
 chown opc:opc /home/opc/.kube/ -R
 
 export KUBECONFIG=~/.kube/config
--- terraform/k3s_infra/cloud-init/scripts/worker.template.sh
+++ terraform/k3s_infra/cloud-init/scripts/worker.template.sh
@@ -2,4 +2,10 @@
 
 systemctl disable firewalld --now
 
+# check tcp to k3s and wait for 10s
+while ! nc -vzw20 server.public.main.oraclevcn.com 6443; do
+  sleep 5
+  echo 'sleep 5'
+done
+
 curl -sfL https://get.k3s.io | K3S_URL=https://server.public.main.oraclevcn.com:6443 K3S_TOKEN='${cluster_token}' sh -
